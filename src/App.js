7d38de854d62826e13a4ec62ba0b8c9bfdd21a30
Tommy Trieu <ttrieu@Tommys-MBP.cable.rcn.com>
2019-07-22 21:08:48 -0400

    Add the GQL query to get all bees

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const client = new ApolloClient({
   uri: 'https://api-useast.graphcms.com/v1/cjydq0tm126y401f6cg9cijkd/master'
-})
+});
+
+const GET_ALL_BEES_QUERY = gql`
+  {
+    bees {
+      name
+      isInHive
+    }
+  }
+`;
 
 function App() {
   return (
